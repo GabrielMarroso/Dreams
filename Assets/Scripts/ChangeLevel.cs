0ab43ecaa69473e585ed47fb87c7abb2e4f3d8e9
Gabriel Marroso <gabrielmarroso@gmail.com>
2022-12-26 15:01:08 -0300

    First Commit
    base game

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLevel : MonoBehaviour
+{
+    public string leveltoload;
+
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && Input.GetKeyDown("z"))
+        {
+            Application.LoadLevel(leveltoload);
+        }
+    }
+
+}
---
+++ Assets/falasTeste.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class falasTeste : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && Input.GetKeyDown("z"))
+        {
+            print("GRRR...");
+        }
+    }
+}
